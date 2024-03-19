9996a1a09f5eed91630226972e26aaaa3ef4b681
ngvdnh <124116162+ngvdnh@users.noreply.github.com>
2024-03-19 22:56:46 +0700

    Update myColor.dart

--- calopilot/lib/models/myColor.dart
+++ calopilot/lib/models/myColor.dart
@@ -7,19 +7,25 @@ class MyUI with ChangeNotifier{
   Color? color3= Color(0xffbfe3f4);
   Color? color4= Color(0xff000001);
   Color? color5= Color(0xff155f82);
-
+  Color? textColor = Color(0xff000000);
+  Color? appBarColor = Color(0xff37718c);
+  
   switchTheme(){
     if(!isDark){
       color1 = Color(0xff262627);
       color2= Color(0xff0c0c0d);
       color3= Color(0xff37718c);
       color4= Color(0xffffffff);
+      textColor = Color(0xffffffff);
+      appBarColor = Color(0xff37718c);
       isDark = true;
     }else{
       color1 = Color(0xffffffff);
       color2= Color(0xfff0f0f1);
       color3= Color(0xffbfe3f4);
       color4= Color(0xff000001);
+      textColor = Color(0xff000001);
+      appBarColor = Color(0xff37718c);
       isDark = false;
     }
     notifyListeners();
