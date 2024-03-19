3a31aa44956f812ae201e111a6dc70da3e402e35
LeAnPhu <155645777+LeAnPhu@users.noreply.github.com>
2024-03-20 01:18:08 +0700

    Add files via upload

---
+++ calopilot/lib/screens/profile.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:provider/provider.dart';
+// ignore: unused_import
+import 'package:calopilot/models/myColor.dart';
+
+class MyProfile extends StatelessWidget {
+  const MyProfile({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MainProfile(),
+    );
+  }
+}
+
+class MainProfile extends StatefulWidget {
+  const MainProfile({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MainProfile createState() => _MainProfile();
+}
+
+class _MainProfile extends State<MainProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Provider.of<MyUI>(context).appBarColor,
+        toolbarHeight: 100.00,
+        title: SafeArea(
+          // ignore: avoid_unnecessary_containers
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: const Row(
+              children: <Widget>[
+                Icon(
+                  Icons.account_circle_rounded,
+                  color: Colors.white,
+                  size: 50,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'User Name', // DOI TEN VOI DATABASE O DAY
+                  style: TextStyle(color: Colors.white),
+                ),
+                Spacer(),
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(
+                      color: Colors.white,
+                      size: 50,
+                      IconData(
+                        0xe57f,
+                        fontFamily: 'MaterialIcons',
+                      )),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 60),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 4,
+                    offset: Offset(4, 8), // Shadow position
+                  ),
+                ],
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Information',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Name:'),
+                          Text('An Le'),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      height: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Gender:'),
+                          Text('Male'),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      height: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Age:'),
+                          Text('21'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 60),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 4,
+                    offset: Offset(4, 8), // Shadow position
+                  ),
+                ],
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'BMI',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('BMI:'),
+                          Text('20.9'),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      height: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Weight:'),
+                          Text('64kg'),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      height: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Height:'),
+                          Text('175cm'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 60),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: const [
+                  BoxShadow(
+                    color: Colors.grey,
+                    blurRadius: 4,
+                    offset: Offset(4, 8), // Shadow position
+                  ),
+                ],
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'DrinkWater',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Divider(
+                      height: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Goal:'),
+                          Text('2200ml'),
+                        ],
+                      ),
+                    ),
+                    Divider(
+                      height: 0.1,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text('Last Drink:'),
+                          Text('7 P.M'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
